8c837a8d5f963ca24aeff934fa6dda11cb73d7e6
abhijith <xperio135@gmail.com>
2023-11-30 18:18:39 +0530

    Initial commit

---
+++ SearchSLL.c
@@ -0,0 +1,100 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct LinkedList
+{
+    int data;
+    struct LinkedList* next;
+}LinkedList;
+LinkedList* first = NULL;
+int isEmpty() {return(first==NULL);}
+LinkedList* createNode(int data)
+{
+    LinkedList* newNode = (LinkedList*)malloc((sizeof(LinkedList)));
+    if(newNode == NULL) 
+    {
+        printf("\nError : Memory Allocation Failed !");
+        exit(0);
+    }
+    newNode->next = NULL;
+    newNode->data = data;
+    return(newNode);
+}
+void insertNodeEnd(int data)
+{
+    LinkedList* newNode = createNode(data);
+    if(isEmpty()) first = newNode;
+    else
+    {
+        LinkedList* temp = first;
+        while(temp->next!=NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+void search(int data)
+{
+    LinkedList*temp = first;
+    int index = 1;
+    while(temp!=NULL)
+    {
+        if(temp->data == data)
+        {
+            printf("Element found at %d\n",index);
+            break;
+        }
+        else
+        {
+            temp = temp->next;
+            index++;
+        }
+    }
+}
+void display()
+{
+    if(isEmpty()) printf("\nError : List Empty !");
+    else
+    {
+        LinkedList*temp = first;
+        printf("\nHead");
+        while(temp!=NULL)
+        {
+            printf("->%d",temp->data);
+            temp=temp->next;
+        }
+    }
+}
+void main()
+{
+    int choice,data,pos;
+    do
+    {
+        printf("Choose an operation : \n");
+        printf("1.Add Node at End\n");
+        printf("2.Search Element\n");
+        printf("3.Display Linked List\n");
+        printf("4.Exit\n");
+        printf("Enter an option : \n");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+                printf("Enter the data to add :\n");
+                scanf("%d",&data);
+                insertNodeEnd(data);
+                break;
+            case 2:
+                printf("Enter the data to search :\n");
+                scanf("%d",&data);
+                search(data);
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                break;
+        }
+    }
+    while(choice<=3);
+}
