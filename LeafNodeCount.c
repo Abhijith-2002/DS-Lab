290ced98da5c30319f8638406613b36841f6ab33
abhijith <xperio135@gmail.com>
2024-02-10 22:30:23 +0530

    initial commit

---
+++ LeafNodeCount.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+}Node;
+Node* createNode(int data)
+{
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+Node* insertNode(Node* root,int data)
+{
+    if(root==NULL)
+    {
+        return createNode(data);
+    }
+    else if(data<root->data)
+    {
+        root->left = insertNode(root->left,data);
+    }
+    else
+    {
+        root->right = insertNode(root->right,data);
+    }
+    return root;
+}
+int leafCount(Node* root)
+{
+    if(root==NULL)
+    {
+        return 0;
+    }
+    else if(root->left==NULL && root->right==NULL)
+    {
+        return 1;
+    }
+    else
+    {
+        return leafCount(root->left)+leafCount(root->right);
+    }
+}
+void main()
+{
+    Node* root = NULL;
+    int data,choice;
+    do
+    {
+        printf("Reading Tree...\n");
+        printf("Enter the node data : \n");
+        scanf("%d",&data);
+        root = insertNode(root,data);
+        printf("Continue adding more nodes?(1/0)\n");
+        scanf("%d",&choice);
+    }while(choice);
+    printf("\nLeaf Count : %d",leafCount(root));
+}
